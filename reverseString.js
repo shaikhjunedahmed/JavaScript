7f3b686e653cb2449211e4e5ec61505357cc61d5
shaikhjunedahmed <shaikhjuned7385@gmail.com>
2024-03-05 20:51:11 +0530

    initial

---
+++ reverseString.js
@@ -0,0 +1,31 @@
+var string = 'Hello World';
+// function revers(str){
+//     let rev=""
+//    for(let ch of str){
+//         rev=ch+rev
+//     }
+//     return rev
+// }
+// console.log(revers(string))
+// function revers(str){
+//     let rev=""
+//     for(let x=str.length-1 ;x>=0;x--){
+//         // console.log(str.length-1)
+//        rev+=str[x]
+//     }
+//     return rev
+// }
+// console.log(revers(string))
+
+
+
+// function revers(str,x){
+//     let count =0
+//   for(let ch in str){
+//       if (str[ch]==x){
+//           count=count+1
+//       }
+//   }
+//   return count
+// }
+// console.log(revers(string,"l"))
